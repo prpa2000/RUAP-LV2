2947ac219f2202799df93887488581acbbf71341
Karla Fehir <kfehir@etfos.hr>
2022-11-07 18:41:29 +0100

    Modifying the Index View to Provide a GUI for Creating Contacts

--- Begin/ContactManager/Controllers/ContactController.cs
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -22,6 +22,14 @@ namespace ContactManager.Controllers
         {
             return this.contactRepository.GetAllContacts();
         }
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
 
     }
 }
